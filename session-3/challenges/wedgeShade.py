b2bd9158bc1ec07e5d1e79f05c90f05d0252c496
David Ross <6578861+djrrb@users.noreply.github.com>
2022-07-21 23:25:47 -0400

    wedge shade

---
+++ session-3/challenges/wedgeShade.py
@@ -0,0 +1,13 @@
+newPage(1200, 350)
+
+fontSize(200)
+font('NickelGothicv2-Wide')
+
+translate(width()/2, 100)
+with savedState():
+    fill(.7)
+    scale(1, .9)
+    skew(-10)
+    text('WORD', (0, 0), align="center")
+
+text('WORD', (0, 0), align="center")
